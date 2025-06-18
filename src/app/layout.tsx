ed15a439bf4eff81e826d515ffa547199b8102fb
wonder1ng <wonder1ng@velog.io>
2025-06-18 20:37:01 +0900

    refactor: imporve responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import { initTokenAutoRefresh } from "@/lib/tokenRefreshManager";
+import { UserActionLoggerClient } from "@/components/UserActionLoggerClient";
+import { useUserActionLogger } from "@/lib/useUserActionLogger";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,22 +23,32 @@ export const metadata: Metadata = {
 
 const pretendard = localFont({
   src: "fonts/PretendardVariable.woff2",
-  display: "swap",
+  display: "block",
   weight: "45 920",
   variable: "--font-pretendard",
 });
+
+const USER_ID = true ? "tester" : "none";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // if (typeof window !== "undefined") {
+  //   // useUserActionLogger({ userId: USER_ID });
+  //   initTokenAutoRefresh();
+  // }
   return (
-    <html lang="ko">
+    <html
+      lang="ko"
+      className={`${pretendard.variable} ${geistSans.variable} ${geistMono.variable} antialiased w-full`}
+    >
       <body
-        // className={`${pretendard.variable} ${geistSans.variable} ${geistMono.variable} antialiased absolute top-0 left-1/2 -translate-x-1/2 w-full`}
-        className={`${pretendard.variable} ${geistSans.variable} ${geistMono.variable} antialiased w-full`}
+        // className={`${pretendard.variable} ${geistSans.variable} ${geistMono.variable} antialiased w-full`}
+        className="subpixel-antialiased w-full"
       >
+        <UserActionLoggerClient userId={USER_ID} />
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full">
           {children}
         </div>
