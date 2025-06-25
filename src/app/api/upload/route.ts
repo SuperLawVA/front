dc81a42aca51de339cc1c22828aab04630d2ee80
wonder1ng <wonder1ng@velog.io>
2025-06-25 22:46:23 +0900

    chore: modify pai path

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,29 +1,43 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// appapi/upload/route.ts
+// import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "nodejs";
+export const POST = async (req: NextRequest) => {
+  try {
+    const formData = await req.formData();
+    const files = formData.getAll("files") as File[];
+    const fileNames = formData.getAll("fileNames") as string[];
 
-export async function POST(request: NextRequest) {
-  const formData = await request.formData();
-  const files = formData.getAll("files") as File[];
+    if (files.length === 0) {
+      return NextResponse.json(
+        { message: "파일이 없습니다." },
+        { status: 400 }
+      );
+    }
 
-  if (files.length === 0) {
-    return NextResponse.json({ error: "파일이 없습니다." }, { status: 400 });
+    // ✅ 예: 파일과 이름 쌍 확인
+
+    const backendFormData = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      backendFormData.append("files", files[i]);
+      backendFormData.append("fileNames", fileNames[i]);
+    }
+    return NextResponse.json({ message: "업로드 성공" }, { status: 200 });
+
+    // const backendRes = await axios.post(
+    //   "https://your-backend/api/upload",
+    //   backendFormData,
+    //   {
+    //     headers: {
+    //       "Content-Type": "multipart/form-data",
+    //     },
+    //   }
+    // );
+
+    // return NextResponse.json(backendRes.data, { status: backendRes.status });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ message: "업로드 실패" }, { status: 500 });
   }
-
-  console.log(
-    "받은 파일 목록:",
-    files.map((f) => ({
-      name: f.name,
-      size: f.size,
-    }))
-  );
-
-  return NextResponse.json({
-    message: "파일 업로드 성공",
-    files: files.map((f) => ({
-      name: f.name,
-      size: f.size,
-    })),
-  });
-}
+};
